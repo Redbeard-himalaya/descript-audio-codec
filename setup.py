c6e39c4476e557a101824485caab41bfbbfa4629
Redbeard-himalaya <82435909+Redbeard-himalaya@users.noreply.github.com>
2025-04-14 20:12:01 +1200

    Update setup.py

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     keywords=["audio", "compression", "machine learning"],
     install_requires=[
         "argbind>=0.3.7",
-        "audiotools@git+https://github.com/Redbeard-himalaya/audiotools#egg=audiotools",
+        "audiotools @ git+https://github.com/Redbeard-himalaya/audiotools",
         "einops",
         "numpy",
         "torch",
